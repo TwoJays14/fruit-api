27202e08c0422fa23894b6872bbe42ce0826fd69
JJatau <jjatau14@outlook.com>
2023-09-12 14:53:03 +0100

    ready for deployment

---
+++ index.js
@@ -0,0 +1,78 @@
+//  Fruit API
+require('dotenv').config()
+const fruits = require('./fruits.json');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const fruitIDs = fruits.map(fruit => fruit.id)
+const highestID = Math.max(...fruitIDs)
+console.log(highestID)
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello Fruit API!");
+})
+
+app.get('/fruits', (req, res) => {
+  res.status(200).send(fruits)
+})
+
+app.get('/fruits/:name', (req, res) => {
+
+  const name = req.params.name.toLowerCase();
+  const fruit = fruits.find(fruit => fruit.name.toLowerCase() === name)
+  if (fruit === undefined) {
+    res.status(404).send("Fruit not found");
+  } else {
+    res.send(fruit)
+  }
+  
+})
+
+const getFruitIndex = name => {
+
+  return fruits.findIndex(fruit => fruit.name.toLowerCase() === name)
+
+}
+
+app.post('/fruits', (req, res) => {
+  
+  const fi = getFruitIndex(req.body.name.toLowerCase());
+  if (fi > -1) {
+    res.status(409).send("The fruit already exists")
+  } else {
+    // Create an array of all ids
+    const fruitIDs = fruits.map(fruit => fruit.id)
+    // Get the maxmium id
+    let highestID = Math.max(...fruitIDs) + 1
+    // Increment max id by one
+    highestID++
+    // Adjust ID to new max ID
+    req.body.id = highestID
+
+    fruits.push(req.body)
+    res.status(201).send(req.body)
+  }
+
+
+  const fruit = req.body
+  console.log(fruit)
+  res.send("New Fruit Created!")
+})
+
+app.delete('/fruits/:name', (req, res) => {
+
+  const fi = getFruitIndex(req.params.name.toLowerCase())
+
+  if ( fi === -1) {
+    res.status(404).send('Fruit not found')
+  } else {
+    fruits.splice(fi, 1)
+    res.sendStatus(200)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`)
+})
